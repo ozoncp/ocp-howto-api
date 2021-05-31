2d36a2f8b0395b2b6b3a78df9013c98db63bcc5d
6axp <levinivan09@yandex.ru>
2021-05-31 14:26:21 +0300

    Simplify tests

--- internal/flusher/flusher_test.go
+++ internal/flusher/flusher_test.go
@@ -12,22 +12,24 @@ import (
 	"github.com/ozoncp/ocp-howto-api/internal/mocks"
 )
 
-var errSome = errors.New("some error")
+var errDummy = errors.New("some error")
+var dummyId = uint64(0)
 
 var _ = Describe("Flusher", func() {
 
 	var (
-		ctrl     *gomock.Controller
-		mockRepo *mocks.MockRepo
-		f        flusher.Flusher
-		howtos   []howto.Howto
+		ctrl          *gomock.Controller
+		mockRepo      *mocks.MockRepo
+		f             flusher.Flusher
+		toFlush       []howto.Howto
+		failedToFlush []howto.Howto
 	)
 
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		mockRepo = mocks.NewMockRepo(ctrl)
 		f = flusher.New(mockRepo)
-		howtos = []howto.Howto{
+		toFlush = []howto.Howto{
 			*howto.New(1, "question0", "answer0"),
 			*howto.New(1, "question1", "answer1"),
 			*howto.New(1, "question2", "answer2"),
@@ -36,42 +38,40 @@ var _ = Describe("Flusher", func() {
 		}
 	})
 
+	JustBeforeEach(func() {
+		failedToFlush = f.Flush(toFlush)
+	})
+
 	AfterEach(func() {
 		ctrl.Finish()
 	})
 
 	Context("Flushed successfully", func() {
+		BeforeEach(func() {
+			mockRepo.EXPECT().AddHowto(gomock.Any()).Return(dummyId, nil).MinTimes(1)
+		})
 		It("", func() {
-			mockRepo.EXPECT().AddHowto(gomock.Any()).Return(uint64(0), nil).MinTimes(1)
-			Expect(f.Flush(howtos)).Should(BeEmpty())
+			Expect(f.Flush(toFlush)).Should(BeEmpty())
 		})
 	})
 
 	Context("Flushed partially", func() {
+		succeeded := 2
+		BeforeEach(func() {
+			mockRepo.EXPECT().AddHowto(gomock.Any()).Return(dummyId, nil).Times(succeeded)
+			mockRepo.EXPECT().AddHowto(gomock.Any()).Return(dummyId, errDummy)
+		})
 		It("", func() {
-			index := 0
-			failOn := 3
-			mockRepo.EXPECT().AddHowto(gomock.Any()).DoAndReturn(
-				func(h howto.Howto) (uint64, error) {
-					if index == failOn {
-						return 0, errSome
-					}
-					index++
-					return h.Id, nil
-				},
-			).MinTimes(1)
-
-			failed := f.Flush(howtos)
-			Expect(failed).ShouldNot(BeEmpty())
-			Expect(len(failed)).Should(BeEquivalentTo(len(howtos) - failOn))
+			Expect(len(failedToFlush)).Should(BeEquivalentTo(len(toFlush) - succeeded))
 		})
 	})
 
 	Context("Flush failed", func() {
+		BeforeEach(func() {
+			mockRepo.EXPECT().AddHowto(gomock.Any()).Return(dummyId, errDummy)
+		})
 		It("", func() {
-			mockRepo.EXPECT().AddHowto(gomock.Any()).Return(uint64(0), errSome)
-			failed := f.Flush(howtos)
-			Expect(failed).Should(BeEquivalentTo(howtos))
+			Expect(failedToFlush).Should(BeEquivalentTo(toFlush))
 		})
 	})
 })
