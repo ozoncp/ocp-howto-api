29ed5112ee8aca69e7b34f6f37e16bdfb502524b
6axp <levinivan09@yandex.ru>
2021-06-07 18:51:40 +0300

    Add startGrpc()

--- cmd/ocp-howto-api/main.go
+++ cmd/ocp-howto-api/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"net"
+
+	api "github.com/ozoncp/ocp-howto-api/internal/api"
+	desc "github.com/ozoncp/ocp-howto-api/pkg/ocp-howto-api"
+	log "github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
+const (
+	grpcPort = 82
+)
+
+func startGrpc(port int) error {
+	address := ":" + fmt.Sprint(port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to start listening: %v", err)
+	}
+
+	server := grpc.NewServer()
+	reflection.Register(server)
+	api := api.NewOcpHowtoApi()
+	desc.RegisterOcpHowtoApiServer(server, api)
+	if err := server.Serve(listener); err != nil {
+		return fmt.Errorf("failed to serve server: %v", err)
+	}
+
+	return nil
+}
 
 func main() {
 	fmt.Println("Howto API. Author: Ivan Levin")
+
+	if err := startGrpc(grpcPort); err != nil {
+		log.Fatal().Msgf("failed to start GRPC server: %v", err)
+	}
 }
