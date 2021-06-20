141b76d08bb38eab715d94a9f365f9a9146c564a
6axp <levinivan09@yandex.ru>
2021-06-20 21:26:35 +0300

    Remove New method for howto.Howto

--- internal/howto/howto.go
+++ internal/howto/howto.go
@@ -9,28 +9,6 @@ type Howto struct {
 	Answer   string
 }
 
-var nextId uint64
-
-func init() {
-	// Get initial value from database or whatever else
-	nextId = 42
-}
-
-func getNextId() uint64 {
-	current := nextId
-	nextId++
-	return current
-}
-
-func New(courseId uint64, question string, answer string) *Howto {
-	return &Howto{
-		getNextId(),
-		courseId,
-		question,
-		answer,
-	}
-}
-
 func (h Howto) String() string {
 	return fmt.Sprintf("Q: %v\r\nA: %v", h.Question, h.Answer)
 }
